7c7ec13a81bbb2dc2815601b6f49447eae4d367f
Samyar <samyar1380.work@gmail.com>
2025-01-15 01:56:16 +0330

    backend

--- AuthenticationPanel.WinApp/Form1.Designer.cs
+++ AuthenticationPanel.WinApp/Form1.Designer.cs
@@ -33,20 +33,20 @@ namespace AuthenticationPanel.WinApp
             this.mainTabControl = new System.Windows.Forms.TabControl();
             this.loginTab = new System.Windows.Forms.TabPage();
             this.linkToSignupTab = new System.Windows.Forms.LinkLabel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.login = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.loginPassword = new System.Windows.Forms.TextBox();
+            this.loginUsername = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.signupTab = new System.Windows.Forms.TabPage();
             this.label7 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.registerPasswordRepeat = new System.Windows.Forms.TextBox();
+            this.signup = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.registerPassword = new System.Windows.Forms.TextBox();
+            this.registerUsername = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.mainTabControl.SuspendLayout();
             this.loginTab.SuspendLayout();
@@ -57,56 +57,62 @@ namespace AuthenticationPanel.WinApp
             // 
             this.mainTabControl.Controls.Add(this.loginTab);
             this.mainTabControl.Controls.Add(this.signupTab);
-            this.mainTabControl.Location = new System.Drawing.Point(123, 31);
+            this.mainTabControl.Location = new System.Drawing.Point(164, 38);
+            this.mainTabControl.Margin = new System.Windows.Forms.Padding(4);
             this.mainTabControl.Name = "mainTabControl";
             this.mainTabControl.SelectedIndex = 0;
-            this.mainTabControl.Size = new System.Drawing.Size(343, 433);
+            this.mainTabControl.Size = new System.Drawing.Size(457, 533);
             this.mainTabControl.TabIndex = 0;
             // 
             // loginTab
             // 
             this.loginTab.BackColor = System.Drawing.Color.Lavender;
             this.loginTab.Controls.Add(this.linkToSignupTab);
-            this.loginTab.Controls.Add(this.button1);
+            this.loginTab.Controls.Add(this.login);
             this.loginTab.Controls.Add(this.label3);
             this.loginTab.Controls.Add(this.label2);
-            this.loginTab.Controls.Add(this.textBox2);
-            this.loginTab.Controls.Add(this.textBox1);
+            this.loginTab.Controls.Add(this.loginPassword);
+            this.loginTab.Controls.Add(this.loginUsername);
             this.loginTab.Controls.Add(this.label1);
-            this.loginTab.Location = new System.Drawing.Point(4, 22);
+            this.loginTab.Location = new System.Drawing.Point(4, 25);
+            this.loginTab.Margin = new System.Windows.Forms.Padding(4);
             this.loginTab.Name = "loginTab";
-            this.loginTab.Padding = new System.Windows.Forms.Padding(3);
-            this.loginTab.Size = new System.Drawing.Size(335, 407);
+            this.loginTab.Padding = new System.Windows.Forms.Padding(4);
+            this.loginTab.Size = new System.Drawing.Size(449, 504);
             this.loginTab.TabIndex = 0;
             this.loginTab.Text = "Login";
             // 
             // linkToSignupTab
             // 
             this.linkToSignupTab.AutoSize = true;
-            this.linkToSignupTab.Location = new System.Drawing.Point(119, 306);
+            this.linkToSignupTab.Location = new System.Drawing.Point(159, 377);
+            this.linkToSignupTab.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.linkToSignupTab.Name = "linkToSignupTab";
-            this.linkToSignupTab.Size = new System.Drawing.Size(103, 13);
+            this.linkToSignupTab.Size = new System.Drawing.Size(124, 16);
             this.linkToSignupTab.TabIndex = 1;
             this.linkToSignupTab.TabStop = true;
             this.linkToSignupTab.Text = "Create new account";
             this.linkToSignupTab.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkToSignupTab_LinkClicked);
             // 
-            // button1
+            // login
             // 
-            this.button1.Location = new System.Drawing.Point(120, 251);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 32);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Login";
-            this.button1.UseVisualStyleBackColor = true;
+            this.login.Location = new System.Drawing.Point(160, 309);
+            this.login.Margin = new System.Windows.Forms.Padding(4);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(133, 39);
+            this.login.TabIndex = 5;
+            this.login.Text = "Login";
+            this.login.UseVisualStyleBackColor = true;
+            this.login.Click += new System.EventHandler(this.login_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(45, 179);
+            this.label3.Location = new System.Drawing.Point(60, 220);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(78, 20);
+            this.label3.Size = new System.Drawing.Size(98, 25);
             this.label3.TabIndex = 4;
             this.label3.Text = "Password";
             // 
@@ -114,33 +120,37 @@ namespace AuthenticationPanel.WinApp
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(45, 131);
+            this.label2.Location = new System.Drawing.Point(60, 161);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(83, 20);
+            this.label2.Size = new System.Drawing.Size(102, 25);
             this.label2.TabIndex = 3;
             this.label2.Text = "Username";
             // 
-            // textBox2
+            // loginPassword
             // 
-            this.textBox2.Location = new System.Drawing.Point(167, 180);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(121, 20);
-            this.textBox2.TabIndex = 2;
+            this.loginPassword.Location = new System.Drawing.Point(223, 222);
+            this.loginPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.loginPassword.Name = "loginPassword";
+            this.loginPassword.Size = new System.Drawing.Size(160, 22);
+            this.loginPassword.TabIndex = 2;
             // 
-            // textBox1
+            // loginUsername
             // 
-            this.textBox1.Location = new System.Drawing.Point(167, 132);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(121, 20);
-            this.textBox1.TabIndex = 1;
+            this.loginUsername.Location = new System.Drawing.Point(223, 162);
+            this.loginUsername.Margin = new System.Windows.Forms.Padding(4);
+            this.loginUsername.Name = "loginUsername";
+            this.loginUsername.Size = new System.Drawing.Size(160, 22);
+            this.loginUsername.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(138, 59);
+            this.label1.Location = new System.Drawing.Point(184, 73);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 25);
+            this.label1.Size = new System.Drawing.Size(80, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "Login";
             // 
@@ -148,17 +158,18 @@ namespace AuthenticationPanel.WinApp
             // 
             this.signupTab.BackColor = System.Drawing.Color.Lavender;
             this.signupTab.Controls.Add(this.label7);
-            this.signupTab.Controls.Add(this.textBox5);
-            this.signupTab.Controls.Add(this.button2);
+            this.signupTab.Controls.Add(this.registerPasswordRepeat);
+            this.signupTab.Controls.Add(this.signup);
             this.signupTab.Controls.Add(this.label4);
             this.signupTab.Controls.Add(this.label5);
-            this.signupTab.Controls.Add(this.textBox3);
-            this.signupTab.Controls.Add(this.textBox4);
+            this.signupTab.Controls.Add(this.registerPassword);
+            this.signupTab.Controls.Add(this.registerUsername);
             this.signupTab.Controls.Add(this.label6);
-            this.signupTab.Location = new System.Drawing.Point(4, 22);
+            this.signupTab.Location = new System.Drawing.Point(4, 25);
+            this.signupTab.Margin = new System.Windows.Forms.Padding(4);
             this.signupTab.Name = "signupTab";
-            this.signupTab.Padding = new System.Windows.Forms.Padding(3);
-            this.signupTab.Size = new System.Drawing.Size(335, 407);
+            this.signupTab.Padding = new System.Windows.Forms.Padding(4);
+            this.signupTab.Size = new System.Drawing.Size(449, 504);
             this.signupTab.TabIndex = 1;
             this.signupTab.Text = "Signup";
             // 
@@ -166,35 +177,40 @@ namespace AuthenticationPanel.WinApp
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(21, 231);
+            this.label7.Location = new System.Drawing.Point(28, 284);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(134, 20);
+            this.label7.Size = new System.Drawing.Size(163, 25);
             this.label7.TabIndex = 13;
             this.label7.Text = "Repeat password";
             // 
-            // textBox5
+            // registerPasswordRepeat
             // 
-            this.textBox5.Location = new System.Drawing.Point(168, 232);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(121, 20);
-            this.textBox5.TabIndex = 12;
+            this.registerPasswordRepeat.Location = new System.Drawing.Point(224, 286);
+            this.registerPasswordRepeat.Margin = new System.Windows.Forms.Padding(4);
+            this.registerPasswordRepeat.Name = "registerPasswordRepeat";
+            this.registerPasswordRepeat.Size = new System.Drawing.Size(160, 22);
+            this.registerPasswordRepeat.TabIndex = 12;
             // 
-            // button2
+            // signup
             // 
-            this.button2.Location = new System.Drawing.Point(118, 286);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 32);
-            this.button2.TabIndex = 11;
-            this.button2.Text = "Sign up";
-            this.button2.UseVisualStyleBackColor = true;
+            this.signup.Location = new System.Drawing.Point(157, 352);
+            this.signup.Margin = new System.Windows.Forms.Padding(4);
+            this.signup.Name = "signup";
+            this.signup.Size = new System.Drawing.Size(133, 39);
+            this.signup.TabIndex = 11;
+            this.signup.Text = "Sign up";
+            this.signup.UseVisualStyleBackColor = true;
+            this.signup.Click += new System.EventHandler(this.signup_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(46, 182);
+            this.label4.Location = new System.Drawing.Point(61, 224);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(78, 20);
+            this.label4.Size = new System.Drawing.Size(98, 25);
             this.label4.TabIndex = 10;
             this.label4.Text = "Password";
             // 
@@ -202,42 +218,47 @@ namespace AuthenticationPanel.WinApp
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(46, 134);
+            this.label5.Location = new System.Drawing.Point(61, 165);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(83, 20);
+            this.label5.Size = new System.Drawing.Size(102, 25);
             this.label5.TabIndex = 9;
             this.label5.Text = "Username";
             // 
-            // textBox3
+            // registerPassword
             // 
-            this.textBox3.Location = new System.Drawing.Point(168, 183);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(121, 20);
-            this.textBox3.TabIndex = 8;
+            this.registerPassword.Location = new System.Drawing.Point(224, 225);
+            this.registerPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.registerPassword.Name = "registerPassword";
+            this.registerPassword.Size = new System.Drawing.Size(160, 22);
+            this.registerPassword.TabIndex = 8;
             // 
-            // textBox4
+            // registerUsername
             // 
-            this.textBox4.Location = new System.Drawing.Point(168, 135);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(121, 20);
-            this.textBox4.TabIndex = 7;
+            this.registerUsername.Location = new System.Drawing.Point(224, 166);
+            this.registerUsername.Margin = new System.Windows.Forms.Padding(4);
+            this.registerUsername.Name = "registerUsername";
+            this.registerUsername.Size = new System.Drawing.Size(160, 22);
+            this.registerUsername.TabIndex = 7;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(126, 62);
+            this.label6.Location = new System.Drawing.Point(168, 76);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(85, 25);
+            this.label6.Size = new System.Drawing.Size(105, 31);
             this.label6.TabIndex = 6;
             this.label6.Text = "Sign up";
             // 
             // mainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(594, 491);
+            this.ClientSize = new System.Drawing.Size(792, 604);
             this.Controls.Add(this.mainTabControl);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "mainForm";
             this.Text = "Authentication Panel";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.set_background);
@@ -257,17 +278,17 @@ namespace AuthenticationPanel.WinApp
         private TabPage signupTab;
         private Label label3;
         private Label label2;
-        private TextBox textBox2;
-        private TextBox textBox1;
+        private TextBox loginPassword;
+        private TextBox loginUsername;
         private Label label1;
-        private Button button1;
+        private Button login;
         private Label label7;
-        private TextBox textBox5;
-        private Button button2;
+        private TextBox registerPasswordRepeat;
+        private Button signup;
         private Label label4;
         private Label label5;
-        private TextBox textBox3;
-        private TextBox textBox4;
+        private TextBox registerPassword;
+        private TextBox registerUsername;
         private Label label6;
         private LinkLabel linkToSignupTab;
     }
--- AuthenticationPanel.WinApp/Form1.cs
+++ AuthenticationPanel.WinApp/Form1.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace AuthenticationPanel.WinApp
 {
@@ -22,19 +19,70 @@ namespace AuthenticationPanel.WinApp
         {
             Graphics graphics = e.Graphics;
 
-            //the rectangle, the same size as our Form
             Rectangle gradient_rectangle = new Rectangle(0, 0, Width, Height);
 
-            //define gradient's properties
             Brush b = new LinearGradientBrush(gradient_rectangle, Color.FromArgb(148, 145, 226), Color.FromArgb(175, 242, 216), 35f);
 
-            //apply gradient         
             graphics.FillRectangle(b, gradient_rectangle);
         }
 
         private void linkToSignupTab_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             mainTabControl.SelectedIndex = 1;
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["AuthDB"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = $"SELECT COUNT(1) FROM User WHERE Username='{loginUsername}' AND Password='{loginPassword}'";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    Console.WriteLine("OK");
+                    
+                    int count = command.ExecuteNonQuery();
+                    if (count == 1)
+                    {
+                        MessageBox.Show("Login successful.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid username or password.");
+                    }
+                }
+            }
+        }
+
+        private void signup_Click(object sender, EventArgs e)
+        {
+            if (registerPassword.Text != registerPasswordRepeat.Text)
+            {
+                MessageBox.Show("Passwords do not match.");
+                return;
+            }
+
+            string connectionString = ConfigurationManager.ConnectionStrings["AuthDB"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = $"SELECT COUNT(1) FROM User WHERE Username='{loginUsername}' AND Password='{loginPassword}'";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+
+                    int count = command.ExecuteNonQuery();
+                    if (count == 0)
+                    {
+                        query = $"INSERT INTO Users (Username, Password) VALUES ('{registerUsername}', '{registerPassword}')";
+                        MessageBox.Show("Account created successfully.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid username or password.");
+                    }
+                }
+            }
         }
     }
 }
